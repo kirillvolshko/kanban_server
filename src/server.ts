482b6171a87c96ebf255639452e9f1ec527e49bc
kyrylovoloshko <k.voloshko04@gmail.com>
2025-04-21 22:32:31 +0300

    change packege.json

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://kanban-beta-seven.vercel.app"],
     credentials: true,
   })
 );
